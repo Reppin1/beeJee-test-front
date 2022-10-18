2a79a419d996c378e7c325654693e69bf8c90622
Andrey Krylov <krylov.a@defilab.space>
2022-10-19 01:07:32 +0300

    hotfix sort

--- src/hooks/useSort/useSort.ts
+++ src/hooks/useSort/useSort.ts
@@ -3,15 +3,29 @@ import { ITaskFields } from "../../store/list/types";
 
 export const useSort = (items: ITaskFields[], sortParams: boolean, sortFiled: SortField | string | undefined) => {
   const sortItems = [...items];
-  if (!sortParams) {
-    return sortItems.sort(function (a, b) {
-      // @ts-ignore
-      return a[sortFiled] > b[sortFiled] ? 1: -1;
-    })
+  if (sortFiled !== 'email') {
+    if (sortParams) {
+      return sortItems.sort(function (a, b) {
+        // @ts-ignore
+        return a[sortFiled] > b[sortFiled] ? 1: -1;
+      })
+    } else {
+      return sortItems.sort(function (a, b) {
+        // @ts-ignore
+        return a[sortFiled] < b[sortFiled] ? 1: -1;
+      })
+    }
   } else {
-    return sortItems.sort(function (a, b) {
-      // @ts-ignore
-      return a[sortFiled] < b[sortFiled] ? 1: -1;
-    })
+    if (!sortParams) {
+      return sortItems.sort(function (a, b) {
+        // @ts-ignore
+        return a[sortFiled] > b[sortFiled] ? 1: -1;
+      })
+    } else {
+      return sortItems.sort(function (a, b) {
+        // @ts-ignore
+        return a[sortFiled] < b[sortFiled] ? 1: -1;
+      })
+    }
   }
 };

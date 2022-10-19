6c5bcb0ef9e9e6f5ead5680207c5223b4da9250b
Andrey Krylov <krylov.a@defilab.space>
2022-10-20 01:45:24 +0300

    hotfix-sort

--- src/hooks/useSort/useSort.ts
+++ src/hooks/useSort/useSort.ts
@@ -3,29 +3,32 @@ import { ITaskFields } from "../../store/list/types";
 
 export const useSort = (items: ITaskFields[], sortParams: boolean, sortFiled: SortField | string | undefined) => {
   const sortItems = [...items];
-  if (sortFiled !== 'email') {
-    if (sortParams) {
-      return sortItems.sort(function (a, b) {
-        // @ts-ignore
-        return a[sortFiled] > b[sortFiled] ? 1: -1;
-      })
+  if (sortFiled) {
+    if (sortFiled !== 'email') {
+      if (sortParams) {
+        return sortItems.sort(function (a, b) {
+          // @ts-ignore
+          return a[sortFiled] > b[sortFiled] ? 1: -1;
+        })
+      } else {
+        return sortItems.sort(function (a, b) {
+          // @ts-ignore
+          return a[sortFiled] < b[sortFiled] ? 1: -1;
+        })
+      }
     } else {
-      return sortItems.sort(function (a, b) {
-        // @ts-ignore
-        return a[sortFiled] < b[sortFiled] ? 1: -1;
-      })
-    }
-  } else {
-    if (!sortParams) {
-      return sortItems.sort(function (a, b) {
-        // @ts-ignore
-        return a[sortFiled] > b[sortFiled] ? 1: -1;
-      })
-    } else {
-      return sortItems.sort(function (a, b) {
-        // @ts-ignore
-        return a[sortFiled] < b[sortFiled] ? 1: -1;
-      })
+      if (!sortParams) {
+        return sortItems.sort(function (a, b) {
+          // @ts-ignore
+          return a[sortFiled] > b[sortFiled] ? 1: -1;
+        })
+      } else {
+        return sortItems.sort(function (a, b) {
+          // @ts-ignore
+          return a[sortFiled] < b[sortFiled] ? 1: -1;
+        })
+      }
     }
   }
+  return sortItems;
 };
